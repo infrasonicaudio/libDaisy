af29881b75b61a9ef2cc8f8d57e41026bb968497
Nick Donaldson <nick@infrasonicaudio.com>
2025-01-22 15:11:16 -0700

    Checkpoint: working UAC2 speaker with replaced hardcoded defs in descriptors

--- Middlewares/Third_Party/tinyusb/config/tusb_config.h
+++ Middlewares/Third_Party/tinyusb/config/tusb_config.h
@@ -94,15 +94,6 @@ extern "C"
 // This is handled by quirk guessing + callbacks - should default to 0 here
 #define CFG_TUD_AUDIO_ENABLE_FEEDBACK_FORMAT_CORRECTION 0
 
-#define CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_RX 2
-
-// Audio format type I specifications
-#define CFG_TUD_AUDIO_FUNC_1_MAX_SAMPLE_RATE 48000
-
-// 16bit in 16bit slots
-#define CFG_TUD_AUDIO_FUNC_1_N_BYTES_PER_SAMPLE_RX 2
-#define CFG_TUD_AUDIO_FUNC_1_RESOLUTION_RX 16
-
 // EP and buffer size - for isochronous EP´s, the buffer and EP size are equal (different sizes would not make sense)
 #define CFG_TUD_AUDIO_ENABLE_EP_OUT 1
 
@@ -112,73 +103,6 @@ extern "C"
                       CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_RX)
 #define CFG_TUD_AUDIO_FUNC_1_EP_OUT_SW_BUF_SZ \
          CFG_TUD_AUDIO_FUNC_1_EP_OUT_SZ_MAX * 8
-// END----
-
-// #define CFG_TUD_AUDIO_FUNC_1_DESC_LEN TUD_AUDIO_DEVICE_STEREO_DESC_LEN
-
-// // How many formats are used, need to adjust USB descriptor if changed
-// #define CFG_TUD_AUDIO_FUNC_1_N_FORMATS 1
-
-// // Audio format type I specifications
-// #define CFG_TUD_AUDIO_FUNC_1_MAX_SAMPLE_RATE 48000
-// #define CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_TX 2
-// #define CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_RX 2
-
-// // 16bit in 16bit slots
-// #define CFG_TUD_AUDIO_FUNC_1_FORMAT_1_N_BYTES_PER_SAMPLE_TX 2
-// #define CFG_TUD_AUDIO_FUNC_1_FORMAT_1_RESOLUTION_TX 16
-// #define CFG_TUD_AUDIO_FUNC_1_FORMAT_1_N_BYTES_PER_SAMPLE_RX 2
-// #define CFG_TUD_AUDIO_FUNC_1_FORMAT_1_RESOLUTION_RX 16
-
-// // EP and buffer size - for isochronous EP´s, the buffer and EP size are equal (different sizes would not make sense)
-// #define CFG_TUD_AUDIO_ENABLE_EP_IN 1
-
-// #define CFG_TUD_AUDIO_FUNC_1_FORMAT_1_EP_SZ_IN                             \
-//     TUD_AUDIO_EP_SIZE(CFG_TUD_AUDIO_FUNC_1_MAX_SAMPLE_RATE,                \
-//                       CFG_TUD_AUDIO_FUNC_1_FORMAT_1_N_BYTES_PER_SAMPLE_TX, \
-//                       CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_TX)
-
-//     /**
-// #define CFG_TUD_AUDIO_FUNC_1_FORMAT_2_EP_SZ_IN                             \
-//     TUD_AUDIO_EP_SIZE(CFG_TUD_AUDIO_FUNC_1_MAX_SAMPLE_RATE,                \
-//                       CFG_TUD_AUDIO_FUNC_1_FORMAT_2_N_BYTES_PER_SAMPLE_TX, \
-//                       CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_TX)
-// */
-
-// // tinyusb FIFOs 4x larger than necessary - helps avoid cb sync errors
-// #define CFG_TUD_AUDIO_FUNC_1_EP_IN_SW_BUF_SZ       \
-//     TU_MAX(CFG_TUD_AUDIO_FUNC_1_FORMAT_1_EP_SZ_IN, \
-//            CFG_TUD_AUDIO_FUNC_1_FORMAT_1_EP_SZ_IN) \
-//         * 4
-
-// #define CFG_TUD_AUDIO_FUNC_1_EP_IN_SZ_MAX       \
-//     TU_MAX(                                     \
-//         CFG_TUD_AUDIO_FUNC_1_FORMAT_1_EP_SZ_IN, \
-//         CFG_TUD_AUDIO_FUNC_1_FORMAT_1_EP_SZ_IN) // Maximum EP IN size for all AS alternate settings used
-
-// // EP and buffer size - for isochronous EP´s, the buffer and EP size are equal (different sizes would not make sense)
-// #define CFG_TUD_AUDIO_ENABLE_EP_OUT 1
-
-// #define CFG_TUD_AUDIO_UNC_1_FORMAT_1_EP_SZ_OUT                             \
-//     TUD_AUDIO_EP_SIZE(CFG_TUD_AUDIO_FUNC_1_MAX_SAMPLE_RATE,                \
-//                       CFG_TUD_AUDIO_FUNC_1_FORMAT_1_N_BYTES_PER_SAMPLE_RX, \
-//                       CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_RX)
-//     /**
-// #define CFG_TUD_AUDIO_UNC_1_FORMAT_2_EP_SZ_OUT                             \
-//     TUD_AUDIO_EP_SIZE(CFG_TUD_AUDIO_FUNC_1_MAX_SAMPLE_RATE,                \
-//                       CFG_TUD_AUDIO_FUNC_1_FORMAT_2_N_BYTES_PER_SAMPLE_RX, \
-//                       CFG_TUD_AUDIO_FUNC_1_N_CHANNELS_RX)
-// */
-
-// #define CFG_TUD_AUDIO_FUNC_1_EP_OUT_SW_BUF_SZ      \
-//     TU_MAX(CFG_TUD_AUDIO_UNC_1_FORMAT_1_EP_SZ_OUT, \
-//            CFG_TUD_AUDIO_UNC_1_FORMAT_1_EP_SZ_OUT) \
-//         * 4
-
-// #define CFG_TUD_AUDIO_FUNC_1_EP_OUT_SZ_MAX      \
-//     TU_MAX(                                     \
-//         CFG_TUD_AUDIO_UNC_1_FORMAT_1_EP_SZ_OUT, \
-//         CFG_TUD_AUDIO_UNC_1_FORMAT_1_EP_SZ_OUT) // Maximum EP IN size for all AS alternate settings used
 
 // Enable feedback EP
 #define CFG_TUD_AUDIO_ENABLE_FEEDBACK_EP 1
